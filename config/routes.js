6dbaba121d8d6a92177ac4a1c4493767b58663dd
Anthony Yonta <atischillin@gmail.com>
2019-12-15 00:47:53 -0500

    added scrapping ability to routes file

--- config/routes.js
+++ config/routes.js
@@ -20,4 +20,100 @@ module.exports = function(router) {
             res.render("saved");
     });
 
+//Get rout for Richmond Times
+router.get("/scrape", function(req, res){
+        // Grab the body of the html with axios
+        axios.get("https://www.richmond.com/search/?f=html&nfl=ap&q=crime&s=start_time&sd=desc&l=25&nsa=eedition").then(function(response) { 
+              
+            // Then, we load that into cheerio and save it to $
+            var $ = cheerio.load(response.data);
+ 
+                //console Log testing
+                // console.log(
+                //         $(".tnt-section-news")
+                //         .children('div')
+                //         .first()
+                //         .children()
+                //         .last()
+                //         .children("div:nth-child(3)")
+                //         .children()
+                //         .children("li")
+                //         .children("time")
+                //         .attr("datetime")
+                //         .html()
+
+                //         );
+                
+        $(".tnt-section-news").each(function(i, element) {
+                var result = {};
+
+                result.headline = $(this)
+                        .children('div')
+                        .first()
+                        .children()
+                        .first()
+                        .children()
+                        .children()
+                        .children()
+                        .children("a")
+                        .children("img")
+                        .attr("alt")
+                result.link = $(this)
+                        .children('div')
+                        .first()
+                        .children()
+                        .first()
+                        .children()
+                        .children()
+                        .children()
+                        .children("a")
+                        .attr("href")
+                result.imgUrl = $(this)
+                        .children('div')
+                        .first()
+                        .children()
+                        .first()
+                        .children()
+                        .children()
+                        .children()
+                        .children("a")
+                        .children("img")
+                        .attr("data-src")
+                result.summary = $(this)
+                        .children('div')
+                        .first()
+                        .children()
+                        .last()
+                        .children("div:nth-child(4)")
+                        .text()
+                        .trim()
+                result.date = $(this)
+                        .children('div')
+                        .first()
+                        .children()
+                        .last()
+                        .children("div:nth-child(3)")
+                        .children()
+                        .children("li")
+                        .children("time")
+                        .attr("datetime")
+                result.displayDate = moment(result.date).format('MMMM Do YYYY');
+                result.tag = "Crime";
+
+                console.log(result);
+                // db.Headline.create(result)
+                        // .then(function(dbHeadline) {
+                        //     // View the added result in the console
+                        //     console.log(dbHeadline);
+                        // })
+                        // .catch(function(err) {
+                        //     // If an error occurred, log it
+                        //     console.log(err);
+                        // });
+            });
+
+
+        });
+
+});
 }
---
+++ scripts/scrape.js
@@ -0,0 +1,7 @@
+//scrape script
+//=============
+
+//Dependencies
+var cheerio = require("cheerio");
+var cheerio = require("request");
+
